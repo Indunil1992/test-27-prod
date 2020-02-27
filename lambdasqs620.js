a6c1f2d7a58dd556a8a888ad260559707b6574a8
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 18:36:48 +0530

    put

--- lambdasqs620.js
+++ lambdasqs620.js
@@ -3,21 +3,14 @@ const sqs = new AWS.SQS();
 
 exports.handler = async (event) => {
     try {
-        let data = await sqs.receiveMessage({
-            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test-queue.fifo`,
-            MaxNumberOfMessages: 1,
-            VisibilityTimeout: 30,
-            WaitTimeSeconds: 0,
-            AttributeNames: ['All']
+        let data = await sqs.sendMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indunil-message-queue`,
+            MessageBody: "c",
+            DelaySeconds: 0
         }).promise();
 
-        console.log("data-receiveMsg");
-        console.log(data);
     } catch (err) {
         // error handling goes here
-        console.log("err-receiveMsg");
-        console.log(err);
-
     };
 
 
